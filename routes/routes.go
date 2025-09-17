ef3d4f54d302748f7a165fe007df80e1584536d4
Vrypt <keriptozzzzz@gmail.com>
2025-09-17 12:52:32 +0700

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	"encoding/json"
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"system-monitor/controllers"
 )
@@ -18,6 +21,31 @@ func SetupRoutes() *mux.Router {
 	api.HandleFunc("/info", controllers.GetSystemInfo).Methods("GET")
 	api.HandleFunc("/gpu", controllers.GetGPUInfo).Methods("GET")
 	api.HandleFunc("/containers", controllers.GetContainerInfo).Methods("GET")
+
+	api.HandleFunc("/all", func(w http.ResponseWriter, r *http.Request) {
+		cpu := controllers.GetCPUInfoData()
+		memory := controllers.GetMemoryInfoData()
+		disk := controllers.GetDiskInfoData()
+		network := controllers.GetNetworkInfoData()
+		processes := controllers.GetProcessInfoData()
+		info := controllers.GetSystemInfoData()
+		gpu := controllers.GetGPUInfoData()
+		containers := controllers.GetContainerInfoData()
+
+		result := map[string]interface{}{
+			"cpu":        cpu,
+			"memory":     memory,
+			"disk":       disk,
+			"network":    network,
+			"processes":  processes,
+			"info":       info,
+			"gpu":        gpu,
+			"containers": containers,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(result)
+	}).Methods("GET")
 
 	r.HandleFunc("/healthz", controllers.Healthz).Methods("GET")
 
